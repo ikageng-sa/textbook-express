2206bacc5d33e2cc365ffbafd695c65dfd833e69
Ikageng Tladi <tladiomphile@gmail.com>
2024-04-13 13:12:30 +0200

    Added an advanced search in the /admin/users/index view

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\AccountStatus;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 
@@ -23,9 +25,12 @@ class UserController extends Controller
         $pageTitle = 'Users';
         $users = User::all(['id', 'name', 'email', 'email_verified_at', 'status', 'created_at']);
 
+        $accountStatuses = array_column(AccountStatus::cases(), 'value');
+
         return view('admin.users.index', [
             'pageTitle' => $pageTitle,
             'users' => $users,
+            'accountStatuses' => $accountStatuses,
         ]);
     }
 
@@ -55,17 +60,39 @@ class UserController extends Controller
 
     }
 
-    public function search(){
+    public function search(Request $request) 
+    {
         $pageTitle = 'Search Results';
-        $query = request('query') ?? '';
+        $query = User::query();
 
-        $users = User::whereAny(['name', 'email', 'status'], 'like', "%$query%")
-            ->get();
+        if(isset($request->text) && $request->text !== null) {
+            $query->whereAny(['name', 'email', 'status'], 'LIKE', "%$request->text%");
+        }
+
+        if(isset($request->status) && $request->status !== null) {
+            $query->whereStatus($request->status);
+        }
+
+        if(isset($request->email_verified) && $request->email_verified !== null) {
+            if($request->email_verified == 'true')
+                $query->whereNotNull('email_verified_at');
+            elseif($request->email_verified == 'false')
+                $query->whereNull('email_verified_at');
+        }
+
+        if(isset($request->created_by) && $request->created_by) {
+            $query->whereBetween('created_at', [$request->created_by, Carbon::now()]);
+        }
+
+        $users = $query->get();
+        
+        $accountStatuses = array_column(AccountStatus::cases(), 'value');
+        session()->flashInput($request->input());
 
         return view('admin.users.index', [
             'pageTitle' => $pageTitle,
             'users' => $users,
-            'query' => $query,
+            'accountStatuses' => $accountStatuses,
         ]);
     }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,22 +2,55 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-
-    </div>
-    <div class="row mb-3">
-        <div class="col-12 d-flex gap-2">
-            <div class="col-4">
-                <form class="" action="{{ route('admin.users.search') }}" method="get">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control" type="text" name="query" value="{{ $query ?? '' }}" placeholder="Enter an email or name...">
-                        <button class="input-group-text btn btn-primary" type="submit"><i class="bi bi-search"></i></button>
+    <div class="row bm-3">
+        <h2>Advanced Search</h2>
+        <div class="col-12">
+            <form action="{{ route('admin.users.search') }}" method="get">
+                <div class="row bm-3">
+                    <div class="col-4">
+                        <div class="">
+                            <input class="form-control form-control-sm" type="text" name="text" value="{{ old('text') }}" placeholder="Enter an email or name...">
+                        </div>
                     </div>
-                </form>
-            </div>
-            <a href="" class="btn btn-sm btn-success">New user <span class="bi bi-plus"></span></a>
+                </div>
+                <div class="row bm-3">
+                    <div class="col-sm-12 col-md-3">
+                        <x-input-label for="">Status</x-input-label>
+                        <select class="form-select form-select-sm" name="status">
+                            <option value="">All</option>
+                            @foreach($accountStatuses as $status)
+                            <option value="{{ $status }}" @if(old('status') == $status) selected @endif>{{ ucfirst($status) }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-sm-12 col-md-3">
+                        <x-input-label for="">Email verified</x-input-label>
+                        <select class="form-select form-select-sm" name="email_verified">
+                            <option value="">All</option>
+                            <option value="true" @if(old('email_verified') == 'true') selected @endif>True</option>
+                            <option value="false" @if(old('email_verified') == 'false') selected @endif>False</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-12 col-md-3">
+                        <x-input-label for="">Created by</x-input-label>
+                        <x-input-text type="date" name="created_by" value="2000/02/22" />
+                    </div>
+                    <div class="col-sm-12 col-md-3"></div>
+                </div>
+                <div class="row bm-3">
+                    <div class="col-sm-12 col-md-9 flex justify-end">
+                        <x-button class="primary btn-sm col-sm-12 col-md-3 col-lg-2">Search</x-button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+    <!-- <div class="row bm-3">
+        <h2>Actions</h2>
+        <div class="col-12 d-flex gap-2">
+            <a href="" class="btn btn-sm btn-success">New user <span class="bi bi-plus"></span></a>
+        </div>
+    </div> -->
     <div class="card">
         <div class="card-header">
             {{ $pageTitle }}
@@ -30,7 +63,7 @@
                         <th scope="col">Name</th>
                         <th scope="col">Email</th>
                         <th scope="col">Role(s)</th>
-                        <th scope="col">Email Verified</th>
+                        <th scope="col" class="center-text">Email Verified</th>
                         <th scope="col">Account Status</th>
                         <th scope="col">Date Created</th>
                         <th></th>
@@ -51,15 +84,15 @@
                             -
                             @endforelse
                         </td>
-                        <td>@isset($user->email_verified_at) <span class="text-success bi bi-check-all"></span> @else <span class="text-danger bi bi-x-lg"></span> @endisset</td>
+                        <td class="center-text">@isset($user->email_verified_at) <span class="text-success bi bi-check-all"></span> @else <span class="text-danger bi bi-x-lg"></span> @endisset</td>
                         <td>{{ ucfirst($user->status) ?? '-' }}</td>
                         <td>{{ $user->created_at ?? '-' }}</td>
                         <td class="d-flex gap-2">
-                        @if (auth()->user()->hasRole('super-admin'))
+                            @if (auth()->user()->hasRole('super-admin'))
                             <a href="{{ route('admin.users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-secondary"><span class="bi bi-pen"></span></a>
-                        @elseif (auth()->user()->id === $user->id || (auth()->user()->hasRole('admin') && !$user->hasRole('admin')))
+                            @elseif (auth()->user()->id === $user->id || (auth()->user()->hasRole('admin') && !$user->hasRole('admin')))
                             <a href="{{ route('admin.users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-secondary"><span class="bi bi-pen"></span></a>
-                        @endif
+                            @endif
 
                         </td>
                     </tr>
