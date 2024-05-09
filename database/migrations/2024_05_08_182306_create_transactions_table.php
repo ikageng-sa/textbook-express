b80ffa3d4c601b5ec0e1420a61e65fa2ba0bfded
Ikageng Tladi <tladiomphile@gmail.com>
2024-05-09 17:59:25 +0200

    Modification: Created new migrations to handle the checkout

---
+++ database/migrations/2024_05_08_182306_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('session')->unique()->nullable();
+            $table->decimal('amount');
+            $table->string('status')->default(OrderStatus::UNPAID);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ database/migrations/2024_05_09_125101_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Transaction;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Transaction::class,'transaction_id')->nullable()->nullOnDelete();
+            $table->foreignId('item_id')->constrained('sales_listings', 'id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
