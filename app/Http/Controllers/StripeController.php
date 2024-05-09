e04d00091659923aa0f683ee7fa5155b7642ebe4
Ikageng Tladi <tladiomphile@gmail.com>
2024-05-09 18:03:58 +0200

    Modification: Added a stripe controller

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\TransactionStatus;
+use App\Enums\SalesListingStatus;
+use App\Models\Order;
+use App\Models\SalesListing;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+
+class StripeController extends Controller
+{
+    public function checkout(SalesListing $book) 
+    {
+
+      Stripe::setApiKey(config('stripe.default.key'));
+      
+      $book = DB::table('books as b')
+          ->select('b.id', 'sl.id as slID', 'b.title', 'b.author', 'b.isbn', 'b.description', 'b.edition', 'b.category', 'b.cover', 'sl.price', 'sl.condition', 'sl.status')
+          ->join('sales_listings as sl', 'sl.book_id', '=', 'b.id')
+          ->where('b.id', '=', $book->id)
+          ->first();
+
+      
+        // Create a new transaction
+        $transaction = Transaction::create([
+          'amount' => $book->price,
+        ]);
+
+        $session =  Session::create([
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'zar',
+                'product_data' => [
+                  'name' => $book->title,
+                ],
+                'unit_amount' => (int) $book->price * 100,
+            ],
+              'quantity' => 1,
+        ]],
+            'mode' => 'payment',
+            'success_url' => route('checkout.success', ['transaction' => $transaction->id]),
+            'cancel_url' => route('checkout.cancel', ['transaction' => $transaction->id]),
+        ]);
+
+        $transaction->update([
+          'session' => $session->id,
+        ]);
+
+        // Record the product order
+        $order = Order::create([
+          'transaction_id' => $transaction->id,
+          'item_id' => $book->id,
+        ]);
+
+        return redirect($session->url);
+            
+    }
+
+    public function success(Transaction $transaction) 
+    {
+
+      // Verify that the page wasn't visited before concerning the same transaction
+      if($transaction->status == TransactionStatus::PAID->value){
+        abort(419);
+      }
+
+      $transaction->update([
+        'status' => TransactionStatus::PAID,
+      ]);
+
+      $orders = Order::whereTransactionId($transaction->id)->get();
+
+      // Change the status of the items related to the transection id to sold
+      foreach( $orders as $order )
+      {
+        SalesListing::whereId($order->item_id)
+          ->update(['status' => SalesListingStatus::SOLD]);
+      }
+
+      return view('general.checkout.success');
+    }
+
+    public function cancel(Transaction $transaction) 
+    {
+      // Verify if page was visited before
+      if($transaction->status == TransactionStatus::CANCELLED->value){
+        abort(419);
+      }
+
+      $transaction->update([
+        'status' => TransactionStatus::CANCELLED,
+      ]);
+
+      return view('general.checkout.cancel');
+    }
+
+    public function webhook()
+    {
+      // TODO
+    }
+}
