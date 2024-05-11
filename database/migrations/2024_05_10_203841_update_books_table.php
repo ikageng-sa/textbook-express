fe233c1c95942e9a2bd002eaf67756e6922ab601
Ikageng Tladi <tladiomphile@gmail.com>
2024-05-11 13:48:33 +0200

    New migration to add a column to the book's table

---
+++ database/migrations/2024_05_10_203841_update_books_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->addColumn('integer', 'quantity')->default(0)->after('reviewed_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->removeColumn('quantity');
+        });
+    }
+};
