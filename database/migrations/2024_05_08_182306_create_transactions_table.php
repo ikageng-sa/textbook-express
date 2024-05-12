ecf9c4f27dccb5fa22cffccdc993c37683789c8c
Ikageng Tladi <tladiomphile@gmail.com>
2024-05-12 20:23:04 +0200

    Added a column to associate transactions with users

--- database/migrations/2024_05_08_182306_create_transactions_table.php
+++ database/migrations/2024_05_08_182306_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('session')->unique()->nullable();
             $table->decimal('amount');
             $table->string('status')->default(TransactionStatus::UNPAID);
