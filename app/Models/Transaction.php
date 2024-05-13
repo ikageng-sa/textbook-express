6d9186339c61f953bf69febff1f50aba0ef2dbab
Ikageng Tladi <tladiomphile@gmail.com>
2024-05-13 16:00:40 +0200

    Refactoring: Added a relationship between each transaction and items/books concerning the transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,14 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Transaction extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'status',
         'amount',
         'session',
     ];
+
+    /**
+     * Transaction record has items associated with it
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\HasMany;
+     */
+    public  function items() : HasMany
+    {
+        return  $this->hasMany(Order::class, 'transaction_id');
+    }
 }
